52377bc8ceaa02537eef282b7173049161513003
harshdwivediiiii <harshvardhandwivedi18@gmail.com>
2025-06-14 13:58:46 +0530

    first commit

---
+++ app/(main)/onboarding/page.jsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
+import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
+import InteractiveBackground from "@/components/Background";
+
+export default async function OnboardingPage() {
+  // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <main>
+      <InteractiveBackground isDarkMode={false} />
+      <OnboardingForm industries={industries} />
+    </main>
+  );
+}
